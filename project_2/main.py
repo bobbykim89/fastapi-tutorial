01613894eaf493a4d1b1df1a0a7ae95fca4741a1
Sihun Kim <skim585@asu.edu>
2024-05-30 01:15:58 -0700

    feat(project_2): add data validation
    add put, delete response and data validation

--- project_2/main.py
+++ project_2/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel, Field
 from typing import Optional
 
@@ -11,13 +11,15 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
 
-    def __init__(self, id, title, author, description, rating) -> None:
+    def __init__(self, id, title, author, description, rating, published_date) -> None:
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 
 class BookRequest(BaseModel):
@@ -26,6 +28,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=-1, lt=6)
+    published_date: int = Field(gt=1999, lt=2031)
 
     class Config:
         json_schema_extra = {
@@ -33,23 +36,24 @@ class BookRequest(BaseModel):
                 'title': 'Harry Pollo and the prisoner of gulag',
                 'author': 'Manguito Lovebird',
                 'description': 'an epic tale',
-                'rating': 5
+                'rating': 5,
+                'published_date': 2018
             }
         }
 
 
 BOOKS = [
     Book(1, 'Python for babies', 'python basics',
-         'too difficult for 1 years old', 2),
+         'too difficult for 1 years old', 2, 2022),
     Book(2, 'How to train your lovebird',
-         'Lovebird Owner', 'very informative', 4),
+         'Lovebird Owner', 'very informative', 4, 2018),
     Book(3, 'Harry pollo and the mystery of disappearing poo',
-         'Manguito Lovebird', 'An epic story of this century', 5),
+         'Manguito Lovebird', 'An epic story of this century', 5, 2021),
     Book(4, 'Harry Pollo and the chamber of millet',
-         'Manguito Lovebird', 'great book', 5),
+         'Manguito Lovebird', 'great book', 5, 2022),
     Book(5, 'Harry Pollo and the order of grackles',
-         'Manguito Lovebird', 'A very nice book', 5),
-    Book(6, 'NodeJS for babies', 'javscript basics', 'It was okay', 3)
+         'Manguito Lovebird', 'A very nice book', 5, 2023),
+    Book(6, 'NodeJS for babies', 'javscript basics', 'It was okay', 3, 2023)
 ]
 
 
@@ -59,17 +63,26 @@ async def get_all_books():
 
 
 @app.get('/books/{book_id}')
-async def read_book(book_id: int):
+async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
 
 
 @app.get('/books/')
-async def read_book_by_rating(book_rating: int):
+async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
     books_to_return = []
     for book in BOOKS:
         if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.get('/books/publish/')
+async def read_books_by_publish_date(published_date: int = Query(gt=1999, lt=2031)):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
             books_to_return.append(book)
     return books_to_return
 
@@ -83,3 +96,18 @@ async def create_book(book_request: BookRequest):
 def find_book_id(book: Book):
     book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
     return book
+
+
+@app.put('/books/update_book')
+async def update_book(book: BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+
+
+@app.delete('/books/{book_id}')
+async def delete_book(book_id: int = Path(gt=0)):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break
