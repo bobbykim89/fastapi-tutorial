48a2af392c5c6e60764e2d78e6b326faec7c1282
Sihun Kim <skim585@asu.edu>
2024-05-29 17:15:58 -0700

    feat(project_2): update pydantic configs, add 2 more get requests

--- project_2/main.py
+++ project_2/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 
@@ -16,6 +18,24 @@ class Book:
         self.author = author
         self.description = description
         self.rating = rating
+
+
+class BookRequest(BaseModel):
+    id: Optional[int] = Field(title='id is not needed', default=None)
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=-1, lt=6)
+
+    class Config:
+        json_schema_extra = {
+            'example': {
+                'title': 'Harry Pollo and the prisoner of gulag',
+                'author': 'Manguito Lovebird',
+                'description': 'an epic tale',
+                'rating': 5
+            }
+        }
 
 
 BOOKS = [
@@ -38,6 +58,28 @@ async def get_all_books():
     return BOOKS
 
 
+@app.get('/books/{book_id}')
+async def read_book(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
+
+@app.get('/books/')
+async def read_book_by_rating(book_rating: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
+
+
 @app.post('/create-book')
-async def create_book(book_request=Body()):
-    BOOKS.append(book_request)
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
