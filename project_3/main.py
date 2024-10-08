80c117acb7d3d0a3894be879d9d22fbd487a7c5d
Sihun Kim <skim585@asu.edu>
2024-10-08 00:32:34 -0700

    feat(project_3): init templates and static files
    initialize templates and setup basic static files

--- project_3/main.py
+++ project_3/main.py
@@ -1,11 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from .models import Base
 from .database import engine
 from .routers import auth, todos, admin, user
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory='project_3/templates')
+
+app.mount('/static', StaticFiles(directory='project_3/static'), name='static')
+
+
+@app.get('/')
+def test(request: Request):
+    return templates.TemplateResponse('home.html', {'request': request})
 
 
 @app.get('/healthy')
